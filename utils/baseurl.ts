9dd7319cebf63412d8d6d0c8e57ca19bc3269587
Vidushi Malik <vidushimalik135@gmail.com>
2026-08-13 16:16:50 +0530

    fixed the bse url

--- utils/baseurl.ts
+++ utils/baseurl.ts
@@ -1,2 +1,2 @@
 // export const API_BASE = "http://localhost:3000";
-export const API_BASE = "https://airesumeanalysis-backend.onrender.com/";
+export const API_BASE = "https://airesumeanalysis-backend.onrender.com";
